f80947f676b5ccdc408f6fb2f27b4a4d85bba23c
ri0u <ri0u@pm.me>
2022-01-07 22:38:28 +0800

    initial
    initial project setup

---
+++ UmbracoTestProject.Core/Controllers/Surface/ContactFormController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using UmbracoTestProject.Core.ViewModels;
+
+namespace UmbracoTestProject.Core.Controllers.Surface
+{
+    public class ContactFormController : SurfaceController
+    {
+        public ActionResult Render()
+        {
+            return PartialView("Forms/ContactForm");
+        }
+
+        [HttpPost]
+        public ActionResult Submit(ContactFormViewModel viewModel)
+        {
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ UmbracoTestProject.Core/ViewModels/ContactFormViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace UmbracoTestProject.Core.ViewModels
+{
+    public class ContactFormViewModel
+    {
+        public string Email { get; set; }
+
+        public string Message { get; set; }
+    }
+}
