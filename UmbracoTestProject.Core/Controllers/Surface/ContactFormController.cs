ee0db3846989b40d91756ba7729a03fa7530a0fb
ri0u <ri0u@pm.me>
2022-02-02 23:38:28 +0800

    added email sending functionality and another textbox for a subject to be included in the email form

--- UmbracoTestProject.Core/Controllers/Surface/ContactFormController.cs
+++ UmbracoTestProject.Core/Controllers/Surface/ContactFormController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Web.Mvc;
 using Umbraco.Web.Mvc;
 using UmbracoTestProject.Core.ViewModels;
+using System.Net.Mail;
 
 namespace UmbracoTestProject.Core.Controllers.Surface
 {
@@ -14,7 +15,21 @@ namespace UmbracoTestProject.Core.Controllers.Surface
         [HttpPost]
         public ActionResult Submit(ContactFormViewModel viewModel)
         {
+            if(ModelState.IsValid)
+            {
+                SendEmail(viewModel);
+                return RedirectToCurrentUmbracoPage();
+            }
             return RedirectToCurrentUmbracoPage();
+        }
+
+        private void SendEmail(ContactFormViewModel viewModel)
+        {
+            MailMessage message = new MailMessage(viewModel.Email, "info@happyporch.com");
+            message.Subject = viewModel.Subject;
+            message.Body = viewModel.Message;
+            SmtpClient client = new SmtpClient("127.0.0.1", 25);
+            client.Send(message);
         }
     }
 }
--- UmbracoTestProject.Core/ViewModels/ContactFormViewModel.cs
+++ UmbracoTestProject.Core/ViewModels/ContactFormViewModel.cs
@@ -1,9 +1,15 @@
-﻿namespace UmbracoTestProject.Core.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UmbracoTestProject.Core.ViewModels
 {
     public class ContactFormViewModel
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
-
+        [Required]
+        public string Subject { get; set; }
+        [Required]
         public string Message { get; set; }
     }
 }
